f25d4add105cbfab3fc3d5785295d309ba10c009
pradeep <ppadmukhi28@gmail.com>
2022-08-01 16:38:53 +0530

    note_app as git repo

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,15 +1,72 @@
 // const fs = require('fs');
 // fs.appendFileSync('node.txt', "\nThis is appendFileSync command");
 
-const validator = require("validator")
-const chalk = require("chalk");
+// const validator = require("validator")
+// const chalk = require("chalk");
+const { version } = require("yargs");
+const yargs = require("yargs");
 
 const notes = require("./notes");
-const note = notes()
-console.log(note)
+const note = notes();
+console.log(note);
 
+// console.log("validating email ", validator.isEmail('pradeep12@gmail.com'))
+// console.log("validating url ", validator.isURL('https://pradeep12@gmail.com'))
+// console.log(chalk.green('Hello world!'));
+// console.log(chalk.blue.inverse.bold('Hello world!'));
 
-console.log("validating email ", validator.isEmail('pradeep12@gmail.com'))
-console.log("validating url ", validator.isURL('https://pradeep12@gmail.com'))
-console.log(chalk.green('Hello world!'));
-console.log(chalk.blue.inverse.bold('Hello world!'));
+// console.log(process.argv[2])
+
+// const command = process.argv[2];
+
+// if (command === 'add') {
+//     console.log("adding the notes")
+// } else if (command === 'remove') {
+//     console.log("removing the notes")
+// }
+
+// Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command using yargs
+yargs.command({
+  command: "add",
+  describe: "add a new note",
+  builder: {
+    title: {
+      describe: "note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+        describe: 'note body',
+        demandOption: true,
+        type: 'string'
+    }
+  },
+
+  handler: function (argv) {
+    console.log("Title: " + argv.title + '\nBody: ' + argv.body);
+  },
+});
+
+// Create remove command using yargs
+yargs.command({
+  command: "remove",
+  describe: "remove a new note",
+  handler: function () {
+    console.log("removing a new note");
+  },
+});
+
+// Create read command using yargs
+yargs.command({
+  command: "read",
+  describe: "read a new note",
+  handler: function (argv) {
+    console.log('reading the notes');
+  },
+});
+
+// console.log(yargs.argv);
+yargs.parse()
