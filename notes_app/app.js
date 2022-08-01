fd4dbd61db606fe6199453b5cbce064a8dba1e82
pradeep <ppadmukhi28@gmail.com>
2022-08-01 13:46:20 +0530

    chalk module used

--- notes_app/app.js
+++ notes_app/app.js
@@ -2,6 +2,7 @@
 // fs.appendFileSync('node.txt', "\nThis is appendFileSync command");
 
 const validator = require("validator")
+const chalk = require("chalk");
 
 const notes = require("./notes");
 const note = notes()
@@ -10,4 +11,5 @@ console.log(note)
 
 console.log("validating email ", validator.isEmail('pradeep12@gmail.com'))
 console.log("validating url ", validator.isURL('https://pradeep12@gmail.com'))
-
+console.log(chalk.green('Hello world!'));
+console.log(chalk.blue.inverse.bold('Hello world!'));
