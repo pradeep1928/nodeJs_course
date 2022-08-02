ea5e258a892d6eaa7b2c7e2027de366a937b1f18
pradeep <ppadmukhi28@gmail.com>
2022-08-02 16:24:25 +0530

    error handling with if-else

--- weather_app/app.js
+++ weather_app/app.js
@@ -3,17 +3,26 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=6e1f29647e7b1289cd33241c5e112c55&query=Mumbai&units=m'
 
 request({url: url, json:true}, (error, res) => {
-    console.log(res.body.current.weather_descriptions[0])
-    console.log(res.body.current.temperature)
+    if (error) {
+        console.log('Unable to connect to weather service.')
+    } else if(res.body.error) {
+        console.log('Unable to find location.')
+    } else {
+        console.log(res.body.current.weather_descriptions[0])
+        console.log(res.body.current.temperature)    
+    }
 })
 
 const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Mumbai.json?access_token=pk.eyJ1IjoicHJhZGVlcDE5MTAiLCJhIjoiY2tvaWk3and2MDBhdzJubWx4Nmt6bWllOCJ9.gsay9c1r0Pj3mC0F5ivnYg'
 
 request({url: geocodeUrl, json:true}, (error, res) => {
-    const longitute = res.body.features[0].center[0];
-    const lattitude = res.body.features[0].center[1];
-
-    console.log(longitute, lattitude)
-
-
+    if (error) {
+        console.log('Unable to connect to location service.')
+    } else if (res.body.features.length == 0) {
+        console.log("Unable to find the location.")
+    } else {
+        const longitute = res.body.features[0].center[0];
+        const lattitude = res.body.features[0].center[1];
+        console.log(longitute, lattitude)    
+    }
 })
