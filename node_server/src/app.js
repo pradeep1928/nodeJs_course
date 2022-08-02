ff7d49a858e4c979b17be879fb19fe5a0741b92b
pradeep <ppadmukhi28@gmail.com>
2022-08-02 20:15:21 +0530

    created express server in node_server

---
+++ node_server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+
+console.log(path.join(__dirname, '../public'))
+
+const publicDirPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.send("this is get")
+})
+
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000')
+})
