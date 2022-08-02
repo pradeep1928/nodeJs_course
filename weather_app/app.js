1b1c814f36f4e97f6c4bc617e5c289e8a512bdf7
pradeep <ppadmukhi28@gmail.com>
2022-08-02 18:00:00 +0530

    use of argv to provide address to geocode and merge of func

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,12 +1,22 @@
-const geoCode = require('./utils/geoCode')
-const forecast = require('./utils/forecast')
+const geoCode = require("./utils/geoCode");
+const forecast = require("./utils/forecast");
 
-geoCode('Mumbai', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data)
-})
+const address = process.argv[2];
 
-forecast('Mumbai', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data)
-})
+if (!address) {
+  console.log("please provide an address");
+} else {
+  geoCode(address, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    forecast(data.location, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
+  });
+}
