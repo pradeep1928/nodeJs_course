a93520a8705a0d4b3dae48c3ee401aad375d9e61
pradeep <ppadmukhi28@gmail.com>
2022-08-02 17:16:59 +0530

    created utils, created func to get geolocation

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geoCode = require('./utils/geoCode')
 
 const url = 'http://api.weatherstack.com/current?access_key=6e1f29647e7b1289cd33241c5e112c55&query=Mumbai&units=m'
 
@@ -13,16 +14,7 @@ request({url: url, json:true}, (error, res) => {
     }
 })
 
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Mumbai.json?access_token=pk.eyJ1IjoicHJhZGVlcDE5MTAiLCJhIjoiY2tvaWk3and2MDBhdzJubWx4Nmt6bWllOCJ9.gsay9c1r0Pj3mC0F5ivnYg'
-
-request({url: geocodeUrl, json:true}, (error, res) => {
-    if (error) {
-        console.log('Unable to connect to location service.')
-    } else if (res.body.features.length == 0) {
-        console.log("Unable to find the location.")
-    } else {
-        const longitute = res.body.features[0].center[0];
-        const lattitude = res.body.features[0].center[1];
-        console.log(longitute, lattitude)    
-    }
+geoCode('Mumbai', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data)
 })
