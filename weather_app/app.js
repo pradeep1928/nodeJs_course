7eba4f23aaeb6958f0a25286f3a9487c68d99e24
pradeep <ppadmukhi28@gmail.com>
2022-08-02 19:03:23 +0530

    destructuring res in geoCode.js, forecast.js

--- weather_app/app.js
+++ weather_app/app.js
@@ -6,16 +6,16 @@ const address = process.argv[2];
 if (!address) {
   console.log("please provide an address");
 } else {
-  geoCode(address, (error, data) => {
+  geoCode(address, (error, {location}) => {
     if (error) {
       return console.log(error);
     }
 
-    forecast(data.location, (error, forecastData) => {
+    forecast(location, (error, forecastData) => {
       if (error) {
         return console.log(error);
       }
-      console.log(data.location);
+      console.log(location);
       console.log(forecastData);
     });
   });
--- weather_app/utils/forecast.js
+++ weather_app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request');
 const forecast = (address, cb) => {
     const url = `http://api.weatherstack.com/current?access_key=6e1f29647e7b1289cd33241c5e112c55&query=${address}&units=m`
 
-    request({url: url, json:true}, (error, res) => {
+    request({url: url, json:true}, (error, {body}) => {
         if (error) {
             cb('Unable to connect to weather service.', undefined)
-        } else if(res.body.error) {
+        } else if(body.error) {
             cb('Unable to find location.', undefined)
         } else {
             cb(undefined, {
-                description: res.body.current.weather_descriptions[0],
-                temperature: res.body.current.temperature
+                description: body.current.weather_descriptions[0],
+                temperature: body.current.temperature
     
             })
         }
