3468c6e5bf18fb243e27e1cf61f8a85a163afbf2
pradeep <ppadmukhi28@gmail.com>
2022-08-02 16:14:25 +0530

    added another url to fetch

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,8 +1,19 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=6e1f29647e7b1289cd33241c5e112c55&query=London,%20United%20Kingdom'
+const url = 'http://api.weatherstack.com/current?access_key=6e1f29647e7b1289cd33241c5e112c55&query=Mumbai&units=m'
 
-request({url: url}, (error, res) => {
-    const data = JSON.parse(res.body);
-    console.log(data.current)
+request({url: url, json:true}, (error, res) => {
+    console.log(res.body.current.weather_descriptions[0])
+    console.log(res.body.current.temperature)
+})
+
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Mumbai.json?access_token=pk.eyJ1IjoicHJhZGVlcDE5MTAiLCJhIjoiY2tvaWk3and2MDBhdzJubWx4Nmt6bWllOCJ9.gsay9c1r0Pj3mC0F5ivnYg'
+
+request({url: geocodeUrl, json:true}, (error, res) => {
+    const longitute = res.body.features[0].center[0];
+    const lattitude = res.body.features[0].center[1];
+
+    console.log(longitute, lattitude)
+
+
 })
