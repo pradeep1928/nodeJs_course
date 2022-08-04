1a9738206494f740608069ba3c57b230ba478178
pradeep <ppadmukhi28@gmail.com>
2022-08-04 20:09:55 +0530

    created get and post api

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -17,6 +17,27 @@ app.post('/users', (req, res) => {
    })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send("user not found")
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+
 app.post('/task', (req, res) => {
     const task = new Task(req.body) 
  
@@ -27,6 +48,27 @@ app.post('/task', (req, res) => {
     })
  })
  
+ app.get('/task', (req, res) => {
+    Task.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+
+app.get('/task/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send("task not found")
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`server is running at port ${port}`)
