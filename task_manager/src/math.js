a84725a97d6a7db1ca38ae8995fd4bffdddd8f49
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-08-07 22:15:16 +0530

    task-manager jest-test

--- task_manager/src/math.js
+++ task_manager/src/math.js
@@ -6,4 +6,16 @@ const celToFar = (temp) => {
     return (temp * 1.8) + 32
 }
 
-module.exports = { farToCel, celToFar }
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                reject("Numbers must be non-negative")
+            }
+            resolve(a + b)
+        }, 1000);
+    })
+}
+
+
+module.exports = { farToCel, celToFar, add }
--- task_manager/tests/math.test.js
+++ task_manager/tests/math.test.js
@@ -1,12 +1,4 @@
-// test("hello world", () => {
-
-// })
-
-// test("this is fail", () => {
-//     throw new Error('failure')
-// })
-
-const {  farToCel, celToFar } = require('../src/math');
+const {  farToCel, celToFar, add } = require('../src/math');
 
 test("fahrenheit to celcius", () => {
     const temp = farToCel(32)
@@ -16,4 +8,20 @@ test("fahrenheit to celcius", () => {
 test("celcius to fahrenheit", () => {
     const temp = celToFar(0)
     expect(temp).toBe(32)
+})
+
+
+// Jest don't wait for asynchronuous process and passes the test even it is wrong
+// So we need to pass the parameter and call that parameter func at the end so jest can wait for it.
+test('asynchronous way', (done) => {
+    setTimeout(() => {
+        expect(1).toBe(1)
+        done()
+    }, 2000);
+})
+
+// we can use async-await for jest test to wait until function executes.
+test("add two numbers async-await", async () => {
+    const sum = await add(10, 12)
+    expect(sum).toBe(22)
 })
