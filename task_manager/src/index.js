ff55b69370f008dd408745c8535f97635b4cafdf
pradeep <ppadmukhi28@gmail.com>
2022-08-09 16:12:21 +0530

    some minor changes in files

--- task_manager/src/index.js
+++ task_manager/src/index.js
@@ -1,5 +1,9 @@
 const app = require('./app')
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send("Welcome to task-manager-app")
+})
 
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
